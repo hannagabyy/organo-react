35afbf87ce83b1956d94f2c05cd67ad7e86cf2f5
hannagabyy <hannarocha000@gmail.com>
2023-10-05 15:30:03 -0300

    Commit inicial - Metade do projeto concluído

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,19 @@
+import './Colaborador.css'
+
+const Colaborador = (props) =>{
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='https://github.com/hannagabyy.png' alt ='Hanna'/>
+            </div>
+            <div className='rodape'>
+                <h4>{props.nome}</h4>
+                <h5>{props.cargo}</h5>
+            </div>
+
+        </div>
+
+    )
+}
+
+export default Colaborador
---
+++ src/componentes/Time/index.js
@@ -0,0 +1,13 @@
+import Colaborador from '../Colaborador'
+import './Time.css'
+
+const Time = (props) =>{
+    const css = {backgroundColor : props.corSecundaria}
+    return(
+        <section className='time'style={css}>
+            <h3 style={{borderColor:props.corPrimaria}} >{props.nome}</h3>
+            {props.colaboradores.map(colaborador => <Colaborador nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem}/>)}
+        </section>
+    )
+}
+export default Time
